a9182a3f17c0bca64ae0c782e370a456f3534699
optitech-games <61181131+optitech-games@users.noreply.github.com>
2021-01-09 19:42:19 +0100

    avslut på dan

--- SAR MS/SAR MS/Controllers/HomeController.cs
+++ SAR MS/SAR MS/Controllers/HomeController.cs
@@ -33,6 +33,12 @@ namespace SAR_MS.Controllers
             return View();
         }
 
+        public IActionResult Skapare()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
